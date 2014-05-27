bf64c8fdbc9d88717111957291edd6e9903bb4f7
Shiphter <stephantl@gmail.com>
2014-05-27 03:51:25 -0700

    Added script for running demos

---
+++ prototype_rmi/bully/startDemo.sh
@@ -0,0 +1,15 @@
+#! /bin/sh
+export CLASSPATH=$CLASSPATH:/home/shiphter/Desktop/ITONK/prototype_rmi/bully/build
+gnome-terminal -x rmiregistry
+sleep 1
+gnome-terminal -x ./startNode.sh
+sleep 1
+gnome-terminal -x ./startNode.sh
+sleep 1
+gnome-terminal -x ./startNode.sh
+sleep 1
+gnome-terminal -x ./startNode.sh
+sleep 1
+gnome-terminal -x ./startNode.sh
+sleep 1
+gnome-terminal -x ./startNode.sh
---
+++ prototype_rmi/ring/startDemo.sh
@@ -0,0 +1,15 @@
+#! /bin/sh
+export CLASSPATH=$CLASSPATH:/home/shiphter/Desktop/ITONK/prototype_rmi/ring/build
+gnome-terminal -x rmiregistry
+sleep 1
+gnome-terminal -x ./startNode.sh
+sleep 1
+gnome-terminal -x ./startNode.sh
+sleep 1
+gnome-terminal -x ./startNode.sh
+sleep 1
+gnome-terminal -x ./startNode.sh
+sleep 1
+gnome-terminal -x ./startNode.sh
+sleep 1
+gnome-terminal -x ./startNode.sh
